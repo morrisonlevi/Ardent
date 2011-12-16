c976b22c76e4e842df3f2608a7cfe1fd0353266b
Levi Morrison <morrison.levi@gmail.com>
2011-12-16 13:55:14 -0700

    Changed tabs to 4 spaces.

--- lib/interface/Stack.php
+++ lib/interface/Stack.php
@@ -2,44 +2,44 @@
 
 interface Stack extends Countable, Iterator {
 
-	/**
-	 * Clears out everything from the stack.
-	 * 
-	 * @return void
-	 */
-	function clear();
+    /**
+     * Clears out everything from the stack.
+     * 
+     * @return void
+     */
+    function clear();
 
-	/**
-	 * Tests if the stack is empty.
-	 * 
-	 * @return bool Returns true if and only if the stack contains no items; false otherwise.
-	 */
-	function isEmpty();
+    /**
+     * Tests if the stack is empty.
+     * 
+     * @return bool Returns true if and only if the stack contains no items; false otherwise.
+     */
+    function isEmpty();
 
-	/**
-	 * Looks at the top item of the stack without removing it.
-	 * 
-	 * @return mixed The item at the top of the stack.
-	 * @throws some exception if the stack is empty.
-	 */
-	function peek();
+    /**
+     * Looks at the top item of the stack without removing it.
+     * 
+     * @return mixed The item at the top of the stack.
+     * @throws some exception if the stack is empty.
+     */
+    function peek();
 
-	/**
-	 * Removes the top item from the stack and returns it.
-	 * 
-	 * @return mixed The item at the top of the stack.
-	 * @throws some exception if the stack is empty.
-	 */
-	function pop();
+    /**
+     * Removes the top item from the stack and returns it.
+     * 
+     * @return mixed The item at the top of the stack.
+     * @throws some exception if the stack is empty.
+     */
+    function pop();
 
-	/**
-	 * Puts an item onto the top of the stack.
-	 * 
-	 * @param mixed $item The item to be pushed onto the stack.
-	 * @return void
-	 */
-	function push($item);
-	
+    /**
+     * Puts an item onto the top of the stack.
+     * 
+     * @param mixed $item The item to be pushed onto the stack.
+     * @return void
+     */
+    function push($item);
+    
 }
 
 ?>
