6f75d495aa26f93a69ff7a217c34a5cf76d5f1e7
Levi Morrison <morrison.levi@gmail.com>
2011-12-16 13:44:06 -0700

    Added descriptions to the methods, and extends Countable and Iterator

--- lib/interface/Stack.php
+++ lib/interface/Stack.php
@@ -1,10 +1,44 @@
 <?php
 
-interface Stack {
+interface Stack extends Countable, Iterator {
 
-	function push();
-	function pop();
+	/**
+	 * Clears out everything from the stack.
+	 * 
+	 * @return void
+	 */
+	function clear();
+
+	/**
+	 * Tests if the stack is empty.
+	 * 
+	 * @return bool Returns true if and only if the stack contains no items; false otherwise.
+	 */
+	function isEmpty();
+
+	/**
+	 * Looks at the top item of the stack without removing it.
+	 * 
+	 * @return mixed The item at the top of the stack.
+	 * @throws some exception if the stack is empty.
+	 */
 	function peek();
+
+	/**
+	 * Removes the top item from the stack and returns it.
+	 * 
+	 * @return mixed The item at the top of the stack.
+	 * @throws some exception if the stack is empty.
+	 */
+	function pop();
+
+	/**
+	 * Puts an item onto the top of the stack.
+	 * 
+	 * @param mixed $item The item to be pushed onto the stack.
+	 * @return void
+	 */
+	function push($item);
 	
 }
 
