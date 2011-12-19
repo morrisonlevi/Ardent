f6f6c985a8eea8ffb13b6a12ab261fe0cc34beef
Levi Morrison <broshizzledizzle@gmail.com>
2011-12-19 12:50:24 -0700

    Added docblock comments to the methods.

--- lib/interface/Collection.php
+++ lib/interface/Collection.php
@@ -6,9 +6,29 @@ namespace Spl;
  * simply a name by which we can give generic properties to.
  */
 interface Collection extends Countable, Iterator {
+    /**
+     * Removes all of the elements from the collection.
+     *
+     * @return void
+     */
     function clear();
+
+    /**
+     * Returns true if the collection contains the given item at least once.
+     * 
+     * @param mixed $item Item whose presence is to be tested.
+     * @return bool Returns true if the collection contains the item.
+     */
     function contains($item);
+
+    /**
+     * Returns true if this collection contains no items.  This is exactly
+     * equivalent to testing count for 0.
+     *
+     * @return bool Returns true if the colleciton contains no items.
+     */
     function isEmpty();
+
 }
 
 ?>
