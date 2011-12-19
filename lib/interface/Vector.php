446287aa7fe6de6d6d00ef728beed63e9062ee14
Levi Morrison <broshizzledizzle@gmail.com>
2011-12-19 12:53:44 -0700

    Initial commit of Vector.

---
+++ lib/interface/Vector.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Spl;
+
+require 'Collection.php';
+
+/**
+ * Vector is a generic container used to represent an array.
+ */
+interface Vector extends Collection, ArrayAccess {
+    
+}
+
+?>
