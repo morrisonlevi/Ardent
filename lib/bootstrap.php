dd38e49442689f414d191b00f176f900a96d97e6
Levi Morrison <morrison.levi@gmail.com>
2011-12-22 19:54:49 -0700

    Commented out the var_dump

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -3,7 +3,7 @@
 spl_autoload_register(function ($class) {
     $path = '/' . str_replace('\\', '/', $class);
     $path = __DIR__ . $path . '.php';
-    var_dump($path);
+//    var_dump($path);
     if (file_exists($path)) {
         require $path;
     }
