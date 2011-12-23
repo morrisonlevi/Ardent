7d9602ea2f6fe1eca0735ac6f754a1d44cc9842d
Levi Morrison <morrison.levi@gmail.com>
2011-12-22 18:46:03 -0700

    Now uses DIRECTORY_SEPERATOR. Removed var_dump.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,9 +1,12 @@
 <?php
 
+if (!defined('DIRECTORY_SEPERATOR')) {
+    define('DIRECTORY_SEPERATOR', '/');
+}
+
 spl_autoload_register(function ($class) {
-    $path = '/' . str_replace('\\', '/', $class);
+    $path = DIRECTORY_SEPERATOR . str_replace('\\', DIRECTORY_SEPERATOR, $class);
     $path = __DIR__ . $path . '.php';
-    var_dump($path);
     if (file_exists($path)) {
         require $path;
     }
