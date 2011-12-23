42e75f4b5d052d20d58d8b4d6f7e38c49aaccf60
Levi Morrison <morrison.levi@gmail.com>
2011-12-22 19:51:38 -0700

    Revert "Now uses DIRECTORY_SEPERATOR. Removed var_dump."
    This reverts commit 7d9602ea2f6fe1eca0735ac6f754a1d44cc9842d.

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -1,12 +1,9 @@
 <?php
 
-if (!defined('DIRECTORY_SEPERATOR')) {
-    define('DIRECTORY_SEPERATOR', '/');
-}
-
 spl_autoload_register(function ($class) {
-    $path = DIRECTORY_SEPERATOR . str_replace('\\', DIRECTORY_SEPERATOR, $class);
+    $path = '/' . str_replace('\\', '/', $class);
     $path = __DIR__ . $path . '.php';
+    var_dump($path);
     if (file_exists($path)) {
         require $path;
     }
