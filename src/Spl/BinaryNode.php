78534928dcd17fe38dee82e7a5a7085a38aea1a8
Levi Morrison <levim@php.net>
2012-08-03 15:50:59 -0600

    Added docblock comments to BinaryNode.

--- src/Spl/BinaryNode.php
+++ src/Spl/BinaryNode.php
@@ -4,16 +4,31 @@ namespace Spl;
 
 class BinaryNode {
 
+    /**
+     * @var BinaryNode
+     */
     protected $left = NULL;
 
+    /**
+     * @var BinaryNode
+     */
     protected $right = NULL;
 
+    /**
+     * @var mixed
+     */
     protected $value = NULL;
 
+    /**
+     * @var int
+     */
     protected $height = 0;
 
-    function __construct($element) {
-        $this->value = $element;
+    /**
+     * @param mixed $value
+     */
+    function __construct($value) {
+        $this->value = $value;
     }
 
     /**
@@ -30,20 +45,35 @@ class BinaryNode {
         return $this->left;
     }
 
+    /**
+     * @param BinaryNode $node
+     * @return void
+     */
     function setRight(BinaryNode $node = NULL) {
         $this->right = $node;
         $this->recalculateHeight();
     }
 
+    /**
+     * @param BinaryNode $node
+     * @return void
+     */
     function setLeft(BinaryNode $node = NULL) {
         $this->left = $node;
         $this->recalculateHeight();
     }
 
+    /**
+     * @return int
+     */
     function getHeight() {
         return $this->height + 1;
     }
 
+    /**
+     * @param BinaryNode $node
+     * @return int
+     */
     protected function getNodeHeight(BinaryNode $node = NULL) {
         if ($node === NULL) {
             return 0;
@@ -52,10 +82,17 @@ class BinaryNode {
         return $node->getHeight();
     }
 
+    /**
+     * @return mixed
+     */
     function getValue() {
         return $this->value;
     }
 
+    /**
+     * @param mixed $value
+     * @return void
+     */
     function setValue($value) {
         $this->value = $value;
     }
@@ -64,10 +101,16 @@ class BinaryNode {
         $this->height = max($this->getNodeHeight($this->left), $this->getNodeHeight($this->right));
     }
 
+    /**
+     * @return bool
+     */
     function isLeaf() {
         return $this->left === NULL && $this->right === NULL;
     }
 
+    /**
+     * @return bool
+     */
     function hasOnlyOneChild() {
         return ($this->left === NULL && $this->right !== NULL)
             || ($this->left !== NULL && $this->right === NULL);
