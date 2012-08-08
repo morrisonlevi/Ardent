1b5d29feaa2515700b64ad149eb086b91f0ec959
Levi Morrison <levim@php.net>
2012-08-08 14:23:39 -0600

    Updated bootstrap to play nicely with other autoloaders.

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,8 @@
 spl_autoload_register(function($class) {
     $ds = DIRECTORY_SEPARATOR;
     $file = __DIR__ . "{$ds}src{$ds}" . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-    include $file;
+
+    if (file_exists($file)) {
+        require $file;
+    }
 });
