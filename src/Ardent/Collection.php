c58dbc3f77a8177638200c7d4985c57ba3506431
Levi Morrison <levim@php.net>
2012-12-15 22:54:29 -0700

    Simplified documentation.

--- src/Ardent/Collection.php
+++ src/Ardent/Collection.php
@@ -6,18 +6,13 @@ use Countable,
     IteratorAggregate;
 
 /**
- * The foundational data structure of this library. It contains methods that
- * should be available to all other data structures in this library.
- *
- * If you look at the interfaces that extend Collection, you'll notice that
- * they often take parameters of Traversable instead of array. This was a
- * design choice to focus on objects instead of doing what is convenient. You
- * can always pass in an ArrayIterator instead.
+ * A collection is countable and has an external iterator. The one helper
+ * method is `isEmpty` which is a frequently used method.
  *
  * Note that Collection does not extend Serializable. This is because all
  * objects can be serialized and unserialized by PHP, and the interface is used
- * only for custom serializing. The final implementations of the data
- * structures in C will extend the Serializable interface.
+ * only for custom serializing. If the extension ever moves to PECL then the
+ * structures will implement Serializable.
  */
 interface Collection extends Countable, IteratorAggregate /*, \Serializable  */ {
 
