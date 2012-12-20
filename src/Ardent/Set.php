63c21ab56464a178d4ccc1d35163e3c259e7728a
Levi Morrison <levim@php.net>
2012-12-20 14:51:28 -0700

    Added `symmetricDifference` to Set.  It is already implemented but I missed adding it to the interface.

--- src/Ardent/Set.php
+++ src/Ardent/Set.php
@@ -43,6 +43,19 @@ interface Set extends Collection {
     function difference(Set $that);
 
     /**
+     * Creates the set that contains the items in the current set that are not
+     * contained in the provided set, as well as items that are in the
+     * provided set that are not in the current set.
+     *
+     * Formally:
+     * A ⊖ B = {x : x ∈ (A \ B) ∨ (B \ A)}
+     *
+     * @param Set $that
+     * @return Set
+     */
+    function symmetricDifference(Set $that);
+
+    /**
      * Creates a new set that contains the items that are in current set that
      * are also contained in the provided set.
      *
