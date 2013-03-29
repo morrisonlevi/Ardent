77dbcad3e751dac371d76c7ab2c2703e968787a8
Levi Morrison <levim@php.net>
2013-03-29 10:55:02 -0600

    More fleshing out of the Trie. STILL HIGHLY UNSTABLE; DO NOT USE.

--- src/Ardent/Trie.php
+++ src/Ardent/Trie.php
@@ -3,6 +3,8 @@
 namespace Ardent;
 
 
+use Ardent\Exception\KeyException;
+
 /**
  * @package Ardent
  *
@@ -13,23 +15,65 @@ class Trie implements \Countable {
 
     private $root = [];
     private $size = 0;
+    private $separator = '';
 
-    function add($key, $value) {
+    function __construct($separator = '') {
+        $this->separator = $separator;
+    }
+
+    function offsetSet($key, $value) {
+        if ($key === NULL) {
+            throw new KeyException('NULL is not allowed as a Trie key');
+        }
+
         $currentLevel =& $this->root;
-        foreach (explode('/', $key) as $segment) {
+
+        $data = $this->split($key);
+
+        foreach ($data as $segment) {
             if (!isset($currentLevel['children'][$segment])) {
                 $currentLevel['children'][$segment] = [];
-                $this->size++;
             }
             $currentLevel =& $currentLevel['children'][$segment];
         }
+
+        if (!array_key_exists('data', $currentLevel)) {
+            // Note that this cannot be done when adding a child to the trie.
+            // Example: your trie is empty and and you add something that is
+            // two levels deep.
+            $this->size++;
+        }
         $currentLevel['data'] = $value;
+    }
+
+
+    function search($key) {
+        if ($key === NULL) {
+            return FALSE;
+        }
+
+        $currentLevel =& $this->root;
+
+        $data = $this->split($key);
+
+        foreach ($data as $segment) {
+            if (!isset($currentLevel['children'][$segment])) {
+                return FALSE;
+            }
+            $currentLevel =& $currentLevel['children'][$segment];
+        }
+        return array_key_exists('data', $currentLevel);
     }
 
     function count() {
         return $this->size;
     }
 
+    private function split($key) {
+        return $this->separator === ''
+            ? str_split($key)
+            : explode($this->separator, $key);
+    }
 
 }
 
