d2e4578a99e1de601f5bff6d9c4ba6107e954491
Levi Morrison <levi_morrison@byu.edu>
2014-04-13 16:44:49 -0600

    Testing fix: `TestCase::provide_rangeN` now properly gives a range from 0 to `$i` instead of always providing 0 to 5.

--- test/Collections/TestCase.php
+++ test/Collections/TestCase.php
@@ -9,7 +9,7 @@ class TestCase extends \PHPUnit_Framework_TestCase {
     function provide_rangeN() {
         $cases = [];
         for ($i = 0; $i < 5; ++$i) {
-            $cases[] = [range(0, 5)];
+            $cases[] = [range(0, $i)];
         }
         return $cases;
     }
