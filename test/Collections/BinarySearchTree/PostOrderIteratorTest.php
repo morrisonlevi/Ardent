edc7a698a59574abe4258c103378f294177a11b7
Levi Morrison <levi_morrison@byu.edu>
2014-04-14 22:09:27 -0600

    Testing: significant code coverage improvement to BinarySearchTree related classes.

---
+++ test/Collections/BinarySearchTree/PostOrderIteratorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Collections;
+
+
+class PostOrderIteratorTest extends TestCase {
+
+
+
+    function instance(BinaryTree $root = null, $count = 0) {
+        return new PostOrderIterator($root, $count);
+    }
+
+
+    function test_empty() {
+        $iterator = $this->instance();
+        $this->assertTrue($iterator->isEmpty());
+    }
+
+
+    function test_toArray() {
+        $tree = new BinaryTree(0);
+        $tree->setLeft(new BinaryTree(-4));
+        $tree->left()->setLeft(new BinaryTree(1));
+        $tree->left()->setRight(new BinaryTree(2));
+
+        $iterator = $this->instance($tree, 4);
+        $expect = [1, 2, -4, 0];
+        $actual = $iterator->toArray();
+        $this->assertEquals($expect, $actual);
+    }
+
+
+} 
---
+++ test/Collections/BinarySearchTree/PreOrderIteratorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Collections;
+
+
+class PreOrderIteratorTest extends TestCase {
+
+
+    function instance(BinaryTree $root = null, $count = 0) {
+        return new PreOrderIterator($root, $count);
+    }
+
+
+    function test_empty() {
+        $iterator = $this->instance();
+        $this->assertTrue($iterator->isEmpty());
+    }
+
+
+    function test_toArray() {
+        $tree = new BinaryTree(0);
+        $tree->setLeft(new BinaryTree(-4));
+        $tree->left()->setLeft(new BinaryTree(1));
+        $tree->left()->setRight(new BinaryTree(2));
+
+        $iterator = $this->instance($tree, 4);
+        $expect = [0, -4, 1, 2];
+        $actual = $iterator->toArray();
+        $this->assertEquals($expect, $actual);
+    }
+
+
+} 
